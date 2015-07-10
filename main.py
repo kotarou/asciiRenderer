4da39cbe454c73102c0dc9c37850078ec9db3473
Kieran Carnegie <kotarou@ecs.vuw.ac.nz>
2015-07-10 13:34:56 +1200

    Use scikit-learn for speed. Broken on linux

--- main.py
+++ main.py
@@ -4,149 +4,125 @@ from scipy import misc
 from scipy import ndimage
 import argparse
 import matplotlib.pyplot as plt
-import Image,ImageDraw,ImageFont
 import random
 import unicodedata
 import os
+import sys
+
+if sys.platform == 'posix':
+	import Image,ImageDraw,ImageFont
+else:
+	# This should also work on *nix machines. Check.
+	from PIL import Image,ImageDraw,ImageFont
 
 import time
+from sklearn.neighbors import KNeighborsClassifier
+
+def edges(image):
+	""" Returns the edges of an image, using the sobel operator in two dimensions"""
+	sx = ndimage.filters.sobel(image, axis=0)
+	sy = ndimage.filters.sobel(image, axis=1)
+	return np.hypot(sx, sy)
+
+def padSides(image, x, y):
+	""" Pads out the edges of an image to divide cleanly"""
+	if y%res != 0:
+		dy = res-(y%res)
+	else:
+		dy = 0
+	if x%res != 0:
+		dx = res-(x%res)
+	else:
+		dx = 0
+	print(dy, dx)
+	image = np.pad(sob,((0,dy),(0,dx)),'constant', constant_values=(0))
+	print("new shape", image.shape)
+	xs = (x/res)+1
+	ys = (y/res)+1
+
+	return np.chararray((ys, xs), unicode=True)
+
+def generateCharBitmap(char, pad=0):
+	""" Given an input string, return an array representing the corresponding bitmap"""
+	# Note that point - pixel conversion means these will all be slightly small for now
+	#print(char)
+	# Special case
+	if char == '':
+		return [0] * res**2
+	x1, y1 = font.getsize(char)
+	a, b = font.getoffset(char)
+	if pad == 0:
+		return np.reshape(np.asarray(font.getmask(char, mode="1")), (y1-b,x1-a))
+	else:
+		aa = np.reshape(np.asarray(font.getmask(char, mode="1")), (y1-b,x1-a))
+		# This is hacky and bad, but for now avoids abberant letters that are too big
+		newX = 0 if x1 - a < 0 or x1 - a > res else x1 - a
+		newY = 0 if y1 - b < 0 or y1 - b > res else y1 - b
+		aa = np.pad( aa , (	(( res-newY )/2, ( res-newX )/2), ((res-newY+1)/2 , (res-newX+1)/2)), 'minimum')
+		# plt.imshow(aa)
+		# plt.show()
+		aa = np.resize(aa, (res**2))
+		return aa
+
+
+
+
 t0 = time.time()
 parser = argparse.ArgumentParser()
-parser.add_argument("fileName", help="display a square of a given number", type=str)
-parser.add_argument("res", help="display a square of a given number", type=int)
-parser.add_argument("version", help="display a square of a given number", type=int)
+parser.add_argument("fileName", help="File to load", type=str)
+parser.add_argument("res", help="Size of pixel square to use", type=int)
+parser.add_argument("hashBins", help="Number of hashing bins. Lower = slower, higher = looks worse but more accurate?", type=int)
 args = parser.parse_args()
 
 image = sp.misc.imread(args.fileName,flatten=True) #WHY DOES THIS WORK!!??
 res = args.res
-version = args.version
+hashs = args.hashBins
 #image = misc.lena()
 
-sx = ndimage.filters.sobel(image, axis=0)
-sy = ndimage.filters.sobel(image, axis=1)
-
-sob = np.hypot(sx, sy)
-avg = np.average(sob)
-
-vals = sob.shape
-y = vals[0]
-x = vals[1]
-print("old shape", sob.shape)
+sob = edges(image)
+y, x =  image.shape
+out = padSides(image, x, y)
+ys, xs = out.shape
 
 
 
-if y%res != 0:
-	dy = res-(y%res)
-else:
-	dy = 0
-if x%res != 0:
-	dx = res-(x%res)
-else:
-	dx = 0
-print(dy, dx)
-sob = np.pad(sob,((0,dy),(0,dx)),'constant', constant_values=(0))
-print("new shape", sob.shape)
-
-xs = (x/res)+1
-ys = (y/res)+1
-
-out = np.chararray((ys, xs), unicode=True)
-print("shape of output ", out.shape)
 
 
-render 	= ['#','|','-','','/','\\', u"\u2610",'(',
-')','{','}','[',']','1','2','3','4','5','6','7',
-'8','9','0','q','w','e','r','t','y','u','i','o',
-'p','a','s','d','f','g','h','j','k','l',';',"'",'z','x','c','v','b','n','m',',',
-'.','/','`','"','!','@','#','$','%','^','&','*','+','=','_',]
+render 	= ['#','|','-','','/','\\', '(', #u"\u2610",
+ ')','{','}','[',']','1','2','3','4','5','6','7',
+ '8','9','0','q','w','e','r','t','y','u','i','o',
+ 'p','a','s','d','f','g','h','j','k','l',';',"'",'z','x','c','v','b','n','m',',',
+ '.','/','`','"','!','@','#','$','%','^','&','*','+','=','_']
 # render = ['#','|','/','\\','-', ' ']
 comp 	= []
-comp2 = []
 
 font = ImageFont.truetype("OpenSans-Regular.ttf", int(res*92/76))
 
-# How about I make this a one off for each time I choose a new resolution?
-# folderDir = 'textRenders_'+str(res)
-# if not os.path.exists(folderDir):
-# 	print("Image folder does not exist")
-# 	os.makedirs(folderDir)
+for index in range(len(render)):
+	x = generateCharBitmap(render[index], 1)
+	#engine.store_vector(x, render[index])
+	comp.append(x)
 
-# 	for target in render:
-# 		canvas = Image.new('L', (res, res), "black")
-# 		draw = ImageDraw.Draw(canvas)
-# 		# This is using <x,y> in contrast to everything else
-# 		draw.text((1,-res/2), target, font = font, fill = "white")
-# 		#canvas.save(folderDir + "/" + target + '.bmp')
-# 		comp.append(canvas)
-# 		# plt.imshow(canvas, cmap='Greys_r')
-# 		# plt.show()
+t1 = time.time()
 
-for target in render:
-	canvas = Image.new('L', (res, res), "black")
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(comp, render)
+#array = generateCharBitmap('R', 1)
 
-	draw = ImageDraw.Draw(canvas)
-	# This is using <x,y> in contrast to everything else
-	draw.text((res/4,-res/3), target, font = font, fill = "white")
-	#canvas.save(folderDir + "/" + target + '.bmp')
-	
-	if version == 1:
-		comp.append(canvas)
-	else:
-		arr2 = np.array(canvas)
-		i = 1
-		result = 0
-		for item in arr2:
-			for entry in item:
-				result += (i* entry / 255.)
-				i += 1
-		#print(result)
-		comp2.append(result)
-
-
-	# plt.imshow(canvas, cmap='Greys_r')
-	# plt.show()
-
-# else:
-# 	print("Image folder exists")
+#print(knn.predict([array]))
 
 
 
 
-def compare(arr):
-	"""Takes an input array and returns the ascii character that best represents it"""
-	mseMax = 999999999
-	use = ''
-
-	i = 1
-	arr2 = err = err2 = 0
-
-	if version != 1:
-		for item in arr:
-			for entry in item:
-				arr2 += (i* entry / 255.)
-				i += 1
-
-	for i in range(0,len(render)):
-		#print(type(comp[i]))
-		if version == 1:
-			err = np.linalg.norm(comp[i] - arr)
-		else:
-			err = np.abs(comp2[i] - arr2)
-
-
-		#print(err)
-
-		if err < mseMax:
-			mseMax = err
-			use = render[i]
-	#print(use)
-	return use
-
+y, x =  image.shape
 for j in xrange(0,y,res):
 	for i in xrange(0,x,res):
 		array = sob[j:j+res, i:i+res]
-		out[(j/res),(i/res)] = compare(array)
-
+		if len(np.hstack(array)) < res**2:
+			array = np.append(array,([0]*(res**2-len(np.hstack(array)))))
+		oute = knn.predict([np.hstack(array)]) #compare2(array)
+		#print('sel: ', oute)
+		out[(j/res),(i/res)] = oute[0]
 canvas2 = Image.new('RGB', (x+100, y+100), (255, 255, 255))
 draw = ImageDraw.Draw(canvas2)
 for j in range(0,ys):
